5b71110a4f49ccc764ddec45a447762864ca64c8
dannyducko <danny@ducko.uk>
2022-06-27 20:17:28 +0100

    remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "hashicorp-learn"
+  # cloud {
+  #   organization = "HoloWolf"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
